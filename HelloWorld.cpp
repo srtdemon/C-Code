ba9836759945d2207abfe74c76f1b9ffbb807609
Mark <SrtDemon.com>
2022-01-06 01:28:23 +0300

    First lesson

---
+++ HelloWorld.cpp
@@ -0,0 +1,41 @@
+// HelloWorld.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+void print()
+{
+    std::cout << "Hello Skillbox\n";
+}
+
+
+
+
+int main()
+{
+    int x = 100;
+    3 + 7;
+    int y = x + 100;
+    int b = 0;
+    b = b + 0;
+
+    int test;
+    test = 100500;
+
+    int test2 = 1005001;
+
+    int mult = x * y;
+    int random = 1002;
+
+    std::cout << random;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
